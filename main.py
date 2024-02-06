55671a40ad56dbb56c8b26db79f1b8b41337640c
Anshuman <57309909+anshumankhunteta@users.noreply.github.com>
2024-02-06 16:07:57 +0530

    searching and error handling improved
    used geonamescache python library to search for city before sending api request

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from flask import Flask,render_template,request,abort
+import geonamescache
 # import json to load json data to python dictionary
 import json
 # urllib.request to make a request to api
@@ -17,27 +18,33 @@ def page_not_found(e):
 
 @app.route('/',methods=['POST','GET'])
 def weather():
-    
+    gc = geonamescache.GeonamesCache()
     api_key = apikey
     
     if request.method == 'POST':
         city = request.form['city']
+        x=gc.search_cities(city, case_sensitive=False, contains_search=True)
+        if len(x)!=0: y=list(x[0].values())
+        else: y=["",""]
+        city = y[1]
     else:
         #for default name kolkata
         city = 'kolkata'
     
-    city = (city.lower()).strip() #remove spaces to avoid errors [Example city : Hong Kong=HongKong]
+    city = (city.lower()).strip() #remove spaces to avoid errors
+
+    placeholder_data = {"country_code": '404 Not Found',}
     
     # json data from api
     try:
         source = urllib.request.urlopen('http://api.openweathermap.org/data/2.5/weather?q=' + city + '&appid='+api_key).read()
     except:
-        return abort(404)
+        return render_template('index.html',data=placeholder_data)
     # converting json data to dictionary
 
     list_of_data = json.loads(source)
 
-    degree_sign = u"\N{DEGREE SIGN}" # importing degree sign for usage
+    degree_sign = u"\N{DEGREE SIGN}" # degree symbol
 
     # data for variable list_of_data
     data = {
